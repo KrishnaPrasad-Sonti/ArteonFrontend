e4969ac1cd03f4a6acffc06a8615091dcbc3f3de
KrishnaPrasad-Sonti <prasadsonti6301@gmail.com>
2024-12-15 12:05:02 +0530

    changes aboutus.css

--- src/components/aboutus.jsx
+++ src/components/aboutus.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './AboutUs.css';
+import './aboutus.css';
 import { FaPaintBrush, FaUserFriends, FaHistory, FaLightbulb } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 
